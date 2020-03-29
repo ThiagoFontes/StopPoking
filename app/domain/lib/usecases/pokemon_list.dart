6ebaecb68fc7f719d0f0e25d4f3140395ac41657
Thiago Fontes <thiago93br@gmail.com>
2020-03-28 22:24:03 -0300

    refactor(domain): Making pokemon list use params

--- app/domain/lib/usecases/pokemon_list.dart
+++ app/domain/lib/usecases/pokemon_list.dart
@@ -1,13 +1,26 @@
 import 'package:domain/entity/pokemon_name_item.dart';
 import 'package:domain/repositories/pokemonrepository.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:core/usecases/usecase.dart';
 
-class GetPokemonList {
+class GetPokemonList implements UseCase<List<PokemonNameItem>, Params> {
   final PokemonRepositoryContract repositoryContract;
 
   GetPokemonList(this.repositoryContract);
 
-  Future<List<PokemonNameItem>> execute({@required int number}) async {
-    return await repositoryContract.getPokemonList(number);
+  @override
+  Future<List<PokemonNameItem>> call(Params params) async {
+    return await repositoryContract.getPokemonList(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params(this.number);
+
+  @override
+  List<Object> get props => [number];
+
 }
