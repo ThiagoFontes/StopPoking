84ab26aad852eae227ce352f986d435bf3d14290
Thiago Fontes <thiago93br@gmail.com>
2020-04-26 13:49:41 -0300

    fix: Closing bloc on test

--- app/presentation/test/bloc/pokemon_list_bloc_test.dart
+++ app/presentation/test/bloc/pokemon_list_bloc_test.dart
@@ -39,7 +39,7 @@ void main() {
       await untilCalled(mockGetPokemonList(any));
 
       verify(mockGetPokemonList(Params(offset: tOffset)));
+      bloc.close();
     });
   });
-  bloc.close();
 }
