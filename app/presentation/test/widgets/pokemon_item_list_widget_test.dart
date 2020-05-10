5f96cbdafec7f6bc327367398c6ba84b1cee9fff
Thiago Fontes <thiago93br@gmail.com>
2020-05-09 22:58:00 -0300

    refactor: Updating widgets tests with predefined resolution

--- app/presentation/test/widgets/pokemon_item_list_widget_test.dart
+++ app/presentation/test/widgets/pokemon_item_list_widget_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:presentation/widgets/pokemon_item_list_widget.dart';
+import 'tester_extensions.dart';
 
 import 'widget_tester_app.dart';
 
 void main() {
   testWidgets('Widget golden test', (WidgetTester tester) async {
+    await tester.setScreenSize(width: 414, height: 736);
     await tester.pumpWidget(
       WidgetTesterApp(
         child: PokemonItemWidget(
